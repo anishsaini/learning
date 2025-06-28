bed0505c1830af9398c2956d8a2d1cc458364ec8
anishsaini <anishoficial@gmail.com>
2025-06-28 21:01:19 +0530

    Hooks

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import Button from "./components/Button.jsx"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Button />
-    <Button/>
+    
     <App />
   </React.StrictMode>
 );
