b8cd0e88e42636a47d2d57adc423eb75bc47fd89
anishsaini <anishoficial@gmail.com>
2025-06-28 13:24:27 +0530

    multi render

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from "./components/Button.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Button />
+    <Button/>
     <App />
   </React.StrictMode>
 );
